9257228a947113f67ef0c5e22439524f24506c7e
henansong <github@nansenho.com>
2021-05-24 17:13:24 +0800

    .

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,16 @@
 import * as commander from 'commander';
+import {translate} from './main';
 
 const program = new commander.Command();
 
 program
     .version('0.0.1')
     .name('ct')
-    .usage('<english>')
-program.parse(process.argv)
+    .usage('<English>')
+    .arguments('<English>')
+    .action(function (english) {
+        console.log('english:' + english)
+        translate('hello')
+    });
+
+program.parse(process.argv);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,44 @@
-console.log('hohoho');
+import * as https from 'https';
+import * as querystring from 'querystring';
+import md5 = require('md5');
+
+export const translate = (word) => {
+    console.log('translate:' + word);
+
+    const appId = '???';
+    const appSecret = '???';
+    const salt = Math.random();
+    const sign = md5(appId + word + salt + appSecret);
+
+    const query: string = querystring.stringify({
+        q: word,
+        from: 'en',
+        to: 'zh',
+        appid: appId,
+        salt: salt,
+        sign: sign
+    });
+
+    return;
+
+    const options = {
+        hostname: 'api.fanyi.baidu.com',
+        port: 443,
+        path: '/api/trans/vip/translate?' + query,
+        method: 'GET'
+    };
+
+    const req = https.request(options, (res) => {
+        console.log('状态码:', res.statusCode);
+        console.log('请求头:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
+};
